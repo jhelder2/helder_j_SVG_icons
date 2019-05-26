abdf6c2e72e800eaefa094bef5779c67d5618e98
unknown <jeffrey.d.helder@gmail.com>
2019-05-25 23:32:09 -0400

    edited -background- id into all SVGs, working on js.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
 	console.log('js. link successful');
 
 	let
-		objectButton = document.querySelector("button");
-		iconBackground = document.querySelector("")
+		objectButton = document.querySelector("button"),
+		iconBackground = document.querySelector("#background");
 
 	function logMyId() {
 		console.log(this.id);
 		this.style.opacity = 0.5;
 	}
 
-	objectButton.addEventListener("click", logMyId);
+	function removeBack(){
+		iconBackground.classList.toggle("hidden");
+	}
+
+	objectButton.addEventListener("click", logMyId, removeBack);
 
 })();
